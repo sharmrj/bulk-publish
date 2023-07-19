7439c103c44fcec2f9069893cf7fbceebd550519
Raghav Sharma <raghavs@adobe.com>
2023-07-19 10:51:59 +0530

    makePostCalls is limited to two threads now

--- app/Main.hs
+++ app/Main.hs
@@ -17,15 +17,17 @@ import Network.HTTP.Simple
 import System.Environment
 import Text.XML.Hexml
 
+main :: IO [()]
 main = do
-  [importReportPath, owner, repo, ref, n] <- getArgs
-  print "Getting paths from Import Report"
+  [importReportPath, owner, repo, ref] <- getArgs
+  putStrLn "Getting paths from Import Report"
   s <- mkEntrySelector "xl/sharedStrings.xml"
   bs <- withArchive importReportPath (getEntry s)
   let paths = getPaths . parse $ bs
-  concurrentlyLimited (read n) (map (\path -> makePOSTCalls [path, owner, repo, ref]) paths)
+  putStrLn "Successfully extracted paths"
+  concurrentlyLimited 2 (map (\path -> makePOSTCalls [path, owner, repo, ref]) paths)
 
-concurrentlyLimited :: Int -> [IO a] -> IO [a]
+concurrentlyLimited :: Integer -> [IO a] -> IO [a]
 concurrentlyLimited n tasks = conc n (zip [0 ..] tasks) [] []
   where
     -- If there are no todos or ongoing tasks
@@ -35,11 +37,11 @@ concurrentlyLimited n tasks = conc n (zip [0 ..] tasks) [] []
       (task, result) <- waitAny ongoing
       conc 1 todo (delete task ongoing) (result : results)
     -- if there are no tasks left
-    conc n [] ongoing results = conc 0 [] ongoing results
+    conc _ [] ongoing results = conc 0 [] ongoing results
     -- Add a new task to the pool (should a resource be available)
-    conc n ((i, x) : xs) ongoing results = do
+    conc num ((i, x) : xs) ongoing results = do
       t <- async $ (i,) <$> x
-      conc (n - 1) xs (t : ongoing) results
+      conc (num - 1) xs (t : ongoing) results
 
 makePOSTCalls :: [String] -> IO ()
 makePOSTCalls args = concurrently_ (post preview " Previewed ") (post publish " Published ")
@@ -48,14 +50,14 @@ makePOSTCalls args = concurrently_ (post preview " Previewed ") (post publish " 
       resp <- tryAny $ f args
       case resp of
         Left e -> putStrLn $ "Failed to" ++ take ((length mes) - 3) mes ++ " " ++ args !! 0 ++ "\n" ++ show e
-        Right resp -> handleBody f resp mes
+        Right r -> handleBody f r mes
     handleBody f resp mes = do
       let statusCode = show $ getResponseStatusCode resp
       case statusCode of
         "200" -> putStrLn $ "Successfully" ++ mes ++ (args !! 0)
         "404" -> putStrLn $ "404 " ++ args !! 0 ++ " not found"
         "503" -> waitAMinuteAndTryAgain f mes
-        otherwise -> print $ statusCode ++ " Failed to" ++ take ((length mes) - 3) mes ++ " " ++ args !! 0
+        _ -> print $ statusCode ++ " Failed to" ++ take ((length mes) - 3) mes ++ " " ++ args !! 0
     waitAMinuteAndTryAgain f mes = do
       putStrLn $ args !! 0 ++ " wasn't" ++ mes ++ ". Trying again after a minute"
       threadDelay $ 60 * 1000000
@@ -97,4 +99,3 @@ preview args = generateRequest args False >>= httpJSONEither
 
 publish :: [String] -> IO (Response (Either JSONException ()))
 publish args = generateRequest args True >>= httpJSONEither
-
